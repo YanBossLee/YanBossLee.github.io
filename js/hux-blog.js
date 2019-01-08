6cda66da5510a0e8e65848d518641d7b3eb735ea
YanBossLee <46086297+YanBossLee@users.noreply.github.com>
2019-01-08 21:47:13 +0800

    Update hux-blog.js

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -27,6 +27,22 @@
 //  $("img").addClass("img-responsive");
 // });
 
+// make all images responsive
+
+// responsive tables
+$(document).ready(function() {
+	$("table").wrap("<div class='table-responsive'></div>");
+	$("table").addClass("table");
+});
+
+// responsive embed videos
+$(document).ready(function () { 
+    $('iframe[src*="youku.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+	$('iframe[src*="tudou.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+});
+
 // Navigation Scripts to Show Header on Scroll-Up
 jQuery(document).ready(function($) {
     var MQL = 1170;
@@ -56,6 +72,9 @@ jQuery(document).ready(function($) {
             });
     }
 });
+
+
+   
 // responsive tables
 $(document).ready(function() {
     $("table").wrap("<div class='table-responsive'></div>");
